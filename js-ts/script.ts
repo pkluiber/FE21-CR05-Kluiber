6b8c6623a40af3d08a6a347d19f3301096615cb1
pkluiber <pkluiber@gmail.com>
2021-10-09 17:46:02 +0200

    FE21-CR-Kluiber

---
+++ js-ts/script.ts
@@ -0,0 +1,50 @@
+
+let result = document.getElementById("cardone");
+  let sigh
+{
+  "sehenw": "The Hainburg Castle",
+  "img": "image/castle.jpg",
+  "links": "https://www.tripadvisor.at/Attraction_Review-g1071606-d10059567-Reviews-The_Hainburg_Castle-Hainburg_an_der_Donau_Lower_Austria.html",
+},{
+
+  "sehenw": "Braunsberg",
+  "img": "image/braunsberg.jpg",
+  "links": "https://www.tripadvisor.at/Attraction_Review-g1071606-d12423299-Reviews-Braunsberg-Hainburg_an_der_Donau_Lower_Austria.html",
+
+}
+
+
+
+let restaurant =`[{
+  
+  "location": "LIVIOS Cafe Bar Gartencenter"
+  "img": "image/livios.jpg
+  "links": "https://livios.at/" 
+},{
+
+  
+  "location": "Karnunt Restaurant"
+  "img": "image/karnut.jpg
+  "links": "https://www.karnunt.at/"
+
+}]`
+
+let events =`[{
+  
+  "name": "Dorothy Khadem-Missagh"
+  "links": "https://www.dorothy-khadem-missagh.com/"
+  "date": "23. Okt. 2021, 18:00 - 20:30 Uhr"
+  "location": "Kulturplatz 1, 2410 Hainburg an der Donau"
+  "price": "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei"
+  "img": "image/Khadem-Missagh.jpg"
+},{
+
+  "name": "Matthias Bartolomey"
+  "links": "https://www.matthiasbartolomey.at/"
+  "date": "27. Nov. 2021, 18:00 - 20:30 Uhr"
+  "location": "Kulturplatz 1, 2410 Hainburg an der Donau"
+  "price": "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei"
+  "img": "image/Bartolomey.jpg"
+
+}]`
+

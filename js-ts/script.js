6e13d230cd5be9510de66cc3ec3fbb5939d307d9
pkluiber <pkluiber@gmail.com>
2021-10-17 12:33:04 +0200

    Kluibertwo

--- js-ts/script.js
+++ js-ts/script.js
@@ -1,48 +1,86 @@
 "use strict";
-let result = document.getElementById("cardone");
-let sigh; {
-    "sehenw";
-    "The Hainburg Castle",
-    "img";
-    "image/castle.jpg",
-    "links";
-    "https://www.tripadvisor.at/Attraction_Review-g1071606-d10059567-Reviews-The_Hainburg_Castle-Hainburg_an_der_Donau_Lower_Austria.html", ;
-} {
-    "sehenw";
-    "Braunsberg",
-    "img";
-    "image/braunsberg.jpg",
-    "links";
-    "https://www.tripadvisor.at/Attraction_Review-g1071606-d12423299-Reviews-Braunsberg-Hainburg_an_der_Donau_Lower_Austria.html", ;
+// Start Sigh
+class sigh {
+    constructor(sighsehenw, sighimg, sighlinks) {
+        this.sighsehenw = sighsehenw;
+        this.sighimg = sighimg;
+        this.sighlinks = sighlinks;
+    }
+    printInfo() {
+        return `<div class="col-sm-6"><div class="card">
+    <img src="${this.sighimg}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">Sight: ${this.sighsehenw}</p>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">Link: ${this.sighlinks}</li>
+
+        </ul>
+    </div>
+  </div>
+  </div>`;
+    }
 }
-let restaurant = `[{
-  
-  "location": "LIVIOS Cafe Bar Gartencenter"
-  "img": "image/livios.jpg
-  "links": "https://livios.at/" 
-},{
+let sigh1 = new sigh("The Hainburg Castle", "image/castle.jpg", "https://www.tripadvisor.at/Attraction_Review-g1071606-d10059567-Reviews-The_Hainburg_Castle-Hainburg_an_der_Donau_Lower_Austria.html");
+let sigh2 = new sigh("Braunsberg", "image/braunsberg.jpg", "https://www.tripadvisor.at/Attraction_Review-g1071606-d12423299-Reviews-Braunsberg-Hainburg_an_der_Donau_Lower_Austria.html");
+let hain = [sigh1, sigh2];
+for (let sigh of hain) {
+    document.getElementById("cardone").innerHTML += sigh.printInfo();
+}
+class restaurant {
+    constructor(location, img, restlink) {
+        this.location = location;
+        this.img = img;
+        this.restlink = restlink;
+    }
+    printInfo() {
+        return `<div class="col-sm-6"><div class="card">
+    <img src="${this.img}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">location: ${this.location}</p>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">Link: ${this.restlink}</li>
 
-  
-  "location": "Karnunt Restaurant"
-  "img": "image/karnut.jpg
-  "links": "https://www.karnunt.at/"
-
-}]`;
-let events = `[{
-  
-  "name": "Dorothy Khadem-Missagh"
-  "links": "https://www.dorothy-khadem-missagh.com/"
-  "date": "23. Okt. 2021, 18:00 - 20:30 Uhr"
-  "location": "Kulturplatz 1, 2410 Hainburg an der Donau"
-  "price": "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei"
-  "img": "image/Khadem-Missagh.jpg"
-},{
-
-  "name": "Matthias Bartolomey"
-  "links": "https://www.matthiasbartolomey.at/"
-  "date": "27. Nov. 2021, 18:00 - 20:30 Uhr"
-  "location": "Kulturplatz 1, 2410 Hainburg an der Donau"
-  "price": "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei"
-  "img": "image/Bartolomey.jpg"
-
-}]`;
+        </ul>
+    </div>
+  </div>
+  </div>`;
+    }
+}
+let rest1 = new restaurant("LIVIOS Cafe Bar Gartencenter", "image/livios.jpg", "https://livios.at/");
+let rest2 = new restaurant("Karnunt Restaurant", "image/karnut.jpg", "https://www.karnunt.at/");
+let rest = [rest1, rest2];
+for (let restaurant of rest) {
+    document.getElementById("cardtwo").innerHTML += restaurant.printInfo();
+}
+class even {
+    constructor(name, eventlink, eventdate, location, eventprice, img) {
+        this.name = name;
+        this.eventlink = eventlink;
+        this.eventdate = eventdate;
+        this.location = location;
+        this.eventprice = eventprice;
+        this.img = img;
+    }
+    printInfo() {
+        return `<div class="col-sm-6"><div class="card">
+    <img src="${this.img}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">Name : ${this.name}</p>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">Location : ${this.location} km</li>
+      <li class="list-group-item">Date : ${this.eventdate}</li>
+      <li class="list-group-item">Price : ${this.eventprice}</li>
+      <li class="list-group-item">Link : ${this.eventlink}</li>
+    </ul>
+    </div>
+  </div>
+  </div>`;
+    }
+}
+let eve1 = new even("Dorothy Khadem-Missagh", "https://www.dorothy-khadem-missagh.com/", "23. Okt. 2021, 18:00 - 20:30 Uhr", "Kulturplatz 1, 2410 Hainburg an der Donau", "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei", "image/Khadem-Missagh.jpg");
+let eve2 = new even("Matthias Bartolomey", "https://www.matthiasbartolomey.at/", "27. Nov. 2021, 18:00 - 20:30 Uhr", "Kulturplatz 1, 2410 Hainburg an der Donau", "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei", "image/Bartolomey.jpg");
+let eve = [eve1, eve2];
+for (let even of eve) {
+    document.getElementById("cardthree").innerHTML += even.printInfo();
+}
+// End Event
--- js-ts/script.ts
+++ js-ts/script.ts
@@ -1,50 +1,156 @@
+// Start Sigh
 
-let result = document.getElementById("cardone");
-  let sigh
-{
-  "sehenw": "The Hainburg Castle",
-  "img": "image/castle.jpg",
-  "links": "https://www.tripadvisor.at/Attraction_Review-g1071606-d10059567-Reviews-The_Hainburg_Castle-Hainburg_an_der_Donau_Lower_Austria.html",
-},{
+interface sighes {
+  sighsehenw: string;
+  sighimg: string;
+  sighlinks: string;
+  
+}
 
-  "sehenw": "Braunsberg",
-  "img": "image/braunsberg.jpg",
-  "links": "https://www.tripadvisor.at/Attraction_Review-g1071606-d12423299-Reviews-Braunsberg-Hainburg_an_der_Donau_Lower_Austria.html",
+class sigh implements sighes {
+  sighsehenw: string;
+  sighimg: string;
+  sighlinks: string;
 
+  constructor (sighsehenw: string,   sighimg: string, sighlinks: string) {
+    this.sighsehenw = sighsehenw; 
+    this.sighimg = sighimg; 
+    this.sighlinks = sighlinks;
+  }
+
+
+
+
+  printInfo () {
+    return `<div class="col-sm-6"><div class="card">
+    <img src="${this.sighimg}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">Sight: ${this.sighsehenw}</p>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">Link: ${this.sighlinks}</li>
+
+        </ul>
+    </div>
+  </div>
+  </div>`
+  }
+}
+let sigh1 = new sigh ("The Hainburg Castle", "image/castle.jpg", "https://www.tripadvisor.at/Attraction_Review-g1071606-d10059567-Reviews-The_Hainburg_Castle-Hainburg_an_der_Donau_Lower_Austria.html");
+
+let sigh2 = new sigh ("Braunsberg", "image/braunsberg.jpg", "https://www.tripadvisor.at/Attraction_Review-g1071606-d12423299-Reviews-Braunsberg-Hainburg_an_der_Donau_Lower_Austria.html");
+
+let hain : Array<sigh> = [sigh1, sigh2];
+for (let sigh of hain) {
+    (document.getElementById("cardone") as HTMLElement).innerHTML += sigh.printInfo();
+}
+// End Sigh
+
+
+// Start Restaurant
+interface restaurants {
+  location: string;
+  img: string;
+  restlink: string;
+  
+}
+
+class restaurant implements restaurants {
+  location: string;
+  img: string;
+  restlink: string;
+
+  constructor (location: string, img: string, restlink: string) {
+    this.location = location; 
+    this.img = img; 
+    this.restlink = restlink;
+  }
+
+  printInfo () {
+    return `<div class="col-sm-6"><div class="card">
+    <img src="${this.img}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">location: ${this.location}</p>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">Link: ${this.restlink}</li>
+
+        </ul>
+    </div>
+  </div>
+  </div>`
+  }
 }
 
 
 
-let restaurant =`[{
+let rest1 = new restaurant ("LIVIOS Cafe Bar Gartencenter", "image/livios.jpg", "https://livios.at/");
+
+let rest2 = new restaurant ("Karnunt Restaurant", "image/karnut.jpg", "https://www.karnunt.at/");
+
+let rest : Array<restaurant> = [rest1, rest2];
+for (let restaurant of rest) {
+    (document.getElementById("cardtwo") as HTMLElement).innerHTML += restaurant.printInfo();
+}
+// End Restaurant 
+
+
+
+
+
+
+// Start Event
+interface events {
+  name: string;
+  link: number;
+  date: number;
+  location: string;
+  price: number;
+  img: string;
   
-  "location": "LIVIOS Cafe Bar Gartencenter"
-  "img": "image/livios.jpg
-  "links": "https://livios.at/" 
-},{
+}
+
+class even implements events {
+  name: string;
+  eventlink: string;
+  eventdate: string;
+  location: string;
+  eventprice: string;
+  img: string;
 
   
-  "location": "Karnunt Restaurant"
-  "img": "image/karnut.jpg
-  "links": "https://www.karnunt.at/"
+  constructor (name: string, eventlink: string, eventdate: string, location: string, eventprice: string, img: string,) {
+      this.name = name;
+      this.eventlink = eventlink;
+      this.eventdate = eventdate;
+      this.location = location;
+      this.eventprice = eventprice;
+      this.img = img;
 
-}]`
-
-let events =`[{
+  }
   
-  "name": "Dorothy Khadem-Missagh"
-  "links": "https://www.dorothy-khadem-missagh.com/"
-  "date": "23. Okt. 2021, 18:00 - 20:30 Uhr"
-  "location": "Kulturplatz 1, 2410 Hainburg an der Donau"
-  "price": "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei"
-  "img": "image/Khadem-Missagh.jpg"
-},{
+  printInfo () {
+    return `<div class="col-sm-6"><div class="card">
+    <img src="${this.img}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">Name : ${this.name}</p>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">Location : ${this.location} km</li>
+      <li class="list-group-item">Date : ${this.eventdate}</li>
+      <li class="list-group-item">Price : ${this.eventprice}</li>
+      <li class="list-group-item">Link : ${this.eventlink}</li>
+    </ul>
+    </div>
+  </div>
+  </div>`}
+}
 
-  "name": "Matthias Bartolomey"
-  "links": "https://www.matthiasbartolomey.at/"
-  "date": "27. Nov. 2021, 18:00 - 20:30 Uhr"
-  "location": "Kulturplatz 1, 2410 Hainburg an der Donau"
-  "price": "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei"
-  "img": "image/Bartolomey.jpg"
+let eve1 = new even ("Dorothy Khadem-Missagh", "https://www.dorothy-khadem-missagh.com/", "23. Okt. 2021, 18:00 - 20:30 Uhr", "Kulturplatz 1, 2410 Hainburg an der Donau", "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei", "image/Khadem-Missagh.jpg");
 
-}]`
+let eve2 = new even ("Matthias Bartolomey", "https://www.matthiasbartolomey.at/", "27. Nov. 2021, 18:00 - 20:30 Uhr", "Kulturplatz 1, 2410 Hainburg an der Donau", "Erwachsene: 30 Euro, Ermäßigt 1: 27 Euro, Jugendliche und Studierende 2: 16 Euro, Kinder bis 6 Jahre: Eintritt frei", "image/Bartolomey.jpg" );
 
+
+let eve : Array<even> = [eve1, eve2];
+for (let even of eve) {
+    (document.getElementById("cardthree") as HTMLElement).innerHTML += even.printInfo();
+  
+  }
+// End Event
